9dfe4e83737df2782585691c5ba8e352aa748fd1
Kristjan Gauk <kristjangauk@gmail.com>
2021-12-18 10:27:38 +0200

    uus
    uss

--- src/main/java/com/example/finalforum/controllers/TopicController.java
+++ src/main/java/com/example/finalforum/controllers/TopicController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.RedirectView;
 
